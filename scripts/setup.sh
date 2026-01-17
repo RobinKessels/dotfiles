419e44a0ebedd7d2238832774d0dc81b5635a78f
Robin Kessels <rk@Robins-MacBook-Air.local>
2026-01-17 13:51:15 +0100

    add link script

---
+++ scripts/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+ln -sfn ~/dotfiles/config/nvim/ ~/.config/nvim
+ln -sfn ~/dotfiles/config/aerospace/aerospace.toml ~/.config/aerospace/aerospace.toml
+ln -sfn ~/dotfiles/config/ghostty/config ~/.config/ghostty/config
+ln -sfn ~/dotfiles/config/zsh/zsh ~/.config/zsh/.zshrc
