9abccbb7ca235040b100cbd2a30dd7fc1ad0803c
Robin Kessels <rk@Robins-MacBook-Air.local>
2026-01-18 14:13:39 +0100

    update config

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,3 +4,8 @@ ln -sfn ~/dotfiles/config/nvim/ ~/.config/nvim
 ln -sfn ~/dotfiles/config/aerospace/aerospace.toml ~/.config/aerospace/aerospace.toml
 ln -sfn ~/dotfiles/config/ghostty/config ~/.config/ghostty/config
 ln -sfn ~/dotfiles/config/zsh/zsh ~/.config/zsh/.zshrc
+
+# Mac specific settings
+# Set key press speed
+defaults write NSGlobalDomain KeyRepeat -int 0
+defaults write NSGlobalDomain InitialKeyRepeat -int 1
