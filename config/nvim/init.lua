c3c0ed0e0586126d81a1b02985ec21d722332f97
Robin Kessels <rk@Robins-MacBook-Air.local>
2026-01-26 19:50:47 +0100

    update config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -4,7 +4,5 @@ require("config.lazy")
 function Transparent(color)
   color = color or "tokyonight"
   vim.cmd.colorscheme(color)
-  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 Transparent()
