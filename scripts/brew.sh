dc1dfbe66cfc07ef1d3f852ae814a75249c9e26f
Robin Kessels <robin@sentigrate.com>
2026-01-30 07:55:39 +0100

    add raycast config

---
+++ scripts/brew.sh
@@ -0,0 +1,14 @@
+#!bin/sh
+
+# Install Homebrew if not already installed
+if ! command -v brew >/dev/null 2>&1; then
+  echo "Homebrew not found. Installing Homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Update Homebrew
+echo "Updating Homebrew..."
+brew update
+brew upgrade
+
+# Install packages from Brewfile
