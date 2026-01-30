dde0db7538b84cf22d90caf8f96f990853b653bb
Robin Kessels <robin@sentigrate.com>
2026-01-30 10:59:11 +0100

    add k9s skin + linearmouse config

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,6 +4,7 @@ ln -sfn ~/dotfiles/config/nvim/ ~/.config/nvim
 ln -sfn ~/dotfiles/config/aerospace/aerospace.toml ~/.config/aerospace/aerospace.toml
 ln -sfn ~/dotfiles/config/ghostty/config ~/.config/ghostty/config
 ln -sfn ~/dotfiles/config/zsh/zsh ~/.config/zsh/.zshrc
+ln -sfn ~/dotfiles/config/k9s/skins ~/.config/k9s/skins
 
 # Mac specific settings
 # Set key press speed
